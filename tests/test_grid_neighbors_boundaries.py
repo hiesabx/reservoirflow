18c69cfcb79f2dd85026d7b3556f57d83454a0d9
Zakariya Abu Grin <zakariya.abugrin@gmail.com>
2022-04-15 14:43:52 +0200

    Update grid neighbors and boundaries test

--- tests/test_grid_neighbors_boundaries.py
+++ tests/test_grid_neighbors_boundaries.py
@@ -9,19 +9,27 @@ class TestApp(unittest.TestCase):
         id = 31
         coords = grid.get_cell_coords(id)
         self.assertEqual(coords, (1, 1, 1))
-        neighbors = grid.get_cell_neighbors(id=id, boundary=False)
+        neighbors = grid.get_cell_neighbors(id=id, boundary=False, fmt='list')
         self.assertEqual(neighbors, [32, 36, 56])
-        boundaries = grid.get_cell_boundaries(id=id)
+        neighbors = grid.get_cell_neighbors(id=id, boundary=False, fmt='dict')
+        self.assertEqual(neighbors, {'x': [32], 'y': [36], 'z': [56]})
+        boundaries = grid.get_cell_boundaries(id=id, fmt='list')
         self.assertEqual(boundaries, [26, 6, 30])
+        boundaries = grid.get_cell_boundaries(id=id, fmt='dict')
+        self.assertEqual(boundaries, {'x': [30], 'y': [26], 'z': [6]})
         
     def test_cell_coords(self):
         coords = (1,1,1)
         id = grid.get_cell_id(coords)
         self.assertEqual(id, 31)
-        neighbors = grid.get_cell_neighbors(coords=coords, boundary=False)
+        neighbors = grid.get_cell_neighbors(coords=coords, boundary=False, fmt='list')
         self.assertEqual(neighbors, [(2, 1, 1), (1, 2, 1), (1, 1, 2)])
-        boundaries = grid.get_cell_boundaries(coords=coords)
+        neighbors = grid.get_cell_neighbors(coords=coords, boundary=False, fmt='dict')
+        self.assertEqual(neighbors, {'x': [(2, 1, 1)], 'y': [(1, 2, 1)], 'z': [(1, 1, 2)]})
+        boundaries = grid.get_cell_boundaries(coords=coords, fmt='list')
         self.assertEqual(boundaries, [(0, 1, 1), (1, 1, 0), (1, 0, 1)])
+        boundaries = grid.get_cell_boundaries(coords=coords, fmt='dict')
+        self.assertEqual(boundaries, {'x': [(0, 1, 1)], 'y': [(1, 0, 1)], 'z': [(1, 1, 0)]})
         
 if __name__ == '__main__':        
     grid = grids.CartGrid(nx=3, ny=3, nz=3, 
