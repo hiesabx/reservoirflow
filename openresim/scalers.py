871381489efbff505b42bf0dcd6d23e33316ee5a
zakgrin <zakariya.abugrin@gmail.com>
2023-05-29 20:50:28 +0200

    Add scalers module

---
+++ openresim/scalers.py
@@ -0,0 +1,6 @@
+"""
+Scaler classes to build scalers for reservoir simulation models.
+
+This module contains all scaler classes used to create a scalers 
+for reservoir simulation model arrays.
+"""
